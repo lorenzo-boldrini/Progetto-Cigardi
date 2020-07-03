47709c0b46b3838645d92ecdc3cbf02d95befa95
lorenzo <lorenzo.boldrini15345@gmail.com>
2020-07-03 16:01:15 +0200

    character controller

---
+++ progetto unity/Assets/Quixel/Megascans/Surfaces/script/movement.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+    public CharacterController controller;
+
+    public float Speed = 12f;
+
+    public float gravity = -9.81f;
+
+    Vector3 velotity;
+    bool IsGrounded;
+
+    public Transform Groundceck;
+    public float GroundDistance = 0.4f;
+    public LayerMask GroundMask;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        IsGrounded = Physics.CheckSphere(Groundceck.position, GroundDistance, GroundMask);
+        if(IsGrounded && velotity.y < 0)
+        {
+            velotity.y = -2f;
+        }
+        float X = Input.GetAxis("Horizontal");
+        float Z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * X + transform.forward * Z;
+
+        controller.Move(move * Speed * Time.deltaTime);
+
+        velotity.y += gravity * 5 * Time.deltaTime;
+        controller.Move(velotity * Time.deltaTime);
+    }
+}
