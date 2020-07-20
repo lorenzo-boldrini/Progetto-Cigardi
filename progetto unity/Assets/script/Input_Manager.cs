5fed6a8bbf2d6474ad572b1ab12447b71ddf4c1c
lorenzo <lorenzo.boldrini15345@gmail.com>
2020-07-20 21:32:28 +0200

    input manager

---
+++ progetto unity/Assets/script/Input_Manager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Input_Manager : MonoBehaviour
+{
+    public static Input_Manager INPUTMAN;
+
+    public UnityEvent Walk;
+    public UnityEvent Run;
+    // Start is called before the first frame update
+
+    private void OnEnable()
+    {
+        INPUTMAN = this;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetAxis("Vertical") != 0 && Input.GetAxis("Run") == 0)
+        {
+            Walk.Invoke();
+        }
+
+        if (Input.GetButton("Run") && Input.GetAxis("Vertical") != 0)
+        {
+            Run.Invoke();
+        }
+    }
+}
--- progetto unity/Assets/script/movement.cs
+++ progetto unity/Assets/script/movement.cs
@@ -22,16 +22,20 @@ public class movement : MonoBehaviour
 
     public FMODUnity.StudioEventEmitter StepSound;
     // Start is called before the first frame update
-    void Start()
+    public void Start()
     {
-
+        Input_Manager.INPUTMAN.Walk.AddListener(Test);
+        Input_Manager.INPUTMAN.Run.AddListener(run);
     }
 
     // Update is called once per frame
-    void Update()
+
+
+   void Test()
     {
+        Speed = 10;
         IsGrounded = Physics.CheckSphere(Groundceck.position, GroundDistance, GroundMask);
-        if(IsGrounded && velotity.y < 0)
+        if (IsGrounded && velotity.y < 0)
         {
             velotity.y = -2f;
         }
@@ -47,7 +51,36 @@ public class movement : MonoBehaviour
 
         _anim.SetFloat("inputX", Z);
         _anim.SetFloat("inputZ", X);
+        _anim.SetBool("Run", false);
+
+
+
     }
 
-   
+    void run()
+    {
+        Speed = 25;
+        IsGrounded = Physics.CheckSphere(Groundceck.position, GroundDistance, GroundMask);
+        if (IsGrounded && velotity.y < 0)
+        {
+            velotity.y = -2f;
+        }
+        float X = Input.GetAxis("Horizontal");
+        float Z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * X + transform.forward * Z;
+
+        controller.Move(move * Speed * Time.deltaTime);
+
+        velotity.y += gravity * Time.deltaTime;
+        controller.Move(velotity * Time.deltaTime);
+
+        _anim.SetFloat("inputX", Z);
+        _anim.SetFloat("inputZ", X);
+        _anim.SetBool("Run", true);
+
+
+
+      
+    }
 }
