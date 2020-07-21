14d72af770bf143da58c93963e258c8b1c01bad0
lorenzo <lorenzo.boldrini15345@gmail.com>
2020-07-21 02:54:05 +0200

    run bar

---
+++ progetto unity/Assets/script/Run_Bar_man.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Run_Bar_man : MonoBehaviour
+{
+    public movement Player;
+    public float Run_counter;
+    float StartNumber;
+    Image _image;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Run_counter = Player.RunBarCounter;
+        StartNumber = Run_counter;
+        _image = GetComponent<Image>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Run_counter = Player.RunBarCounter;
+        _image.fillAmount = Run_counter / StartNumber;
+    }
+}
--- progetto unity/Assets/script/movement.cs
+++ progetto unity/Assets/script/movement.cs
@@ -19,22 +19,48 @@ public class movement : MonoBehaviour
     public float GroundDistance = 0.4f;
     public LayerMask GroundMask;
 
+    public float RunBarCounter = 20;
 
-    public FMODUnity.StudioEventEmitter StepSound;
+
     // Start is called before the first frame update
     public void Start()
     {
         Input_Manager.INPUTMAN.Walk.AddListener(Test);
-        Input_Manager.INPUTMAN.Run.AddListener(run);
     }
 
     // Update is called once per frame
 
-
-   void Test()
+    private void Update()
     {
-        Speed = 10;
-        IsGrounded = Physics.CheckSphere(Groundceck.position, GroundDistance, GroundMask);
+        Debug.Log(RunBarCounter);
+
+        if (Input.GetButton("Run") == false && RunBarCounter <= 20)
+        {
+            Debug.Log("Riprendi il fiato");
+            RunBarCounter += 1 * Time.deltaTime;
+            if (Input.GetAxis("Vertical") == 0 && Input.GetAxis("Horizontal") == 0)
+                RunBarCounter += 1 * Time.deltaTime;
+        }
+    }
+
+    void Test()
+    {
+        if (Input.GetAxis("Vertical") != 0 && Input.GetAxis("Run") == 0)
+        {
+
+            Speed = 10;
+            _anim.SetBool("Run", false);
+        }
+        if (Input.GetButton("Run") && Input.GetAxis("Vertical") != 0)
+        {
+            RunBarCounter -= 1 * Time.deltaTime;
+            _anim.SetBool("Run", true);
+            Speed = 20;
+        }
+
+
+
+            IsGrounded = Physics.CheckSphere(Groundceck.position, GroundDistance, GroundMask);
         if (IsGrounded && velotity.y < 0)
         {
             velotity.y = -2f;
@@ -51,36 +77,10 @@ public class movement : MonoBehaviour
 
         _anim.SetFloat("inputX", Z);
         _anim.SetFloat("inputZ", X);
-        _anim.SetBool("Run", false);
 
 
 
     }
 
-    void run()
-    {
-        Speed = 25;
-        IsGrounded = Physics.CheckSphere(Groundceck.position, GroundDistance, GroundMask);
-        if (IsGrounded && velotity.y < 0)
-        {
-            velotity.y = -2f;
-        }
-        float X = Input.GetAxis("Horizontal");
-        float Z = Input.GetAxis("Vertical");
-
-        Vector3 move = transform.right * X + transform.forward * Z;
-
-        controller.Move(move * Speed * Time.deltaTime);
-
-        velotity.y += gravity * Time.deltaTime;
-        controller.Move(velotity * Time.deltaTime);
-
-        _anim.SetFloat("inputX", Z);
-        _anim.SetFloat("inputZ", X);
-        _anim.SetBool("Run", true);
-
-
-
-      
-    }
+   
 }
