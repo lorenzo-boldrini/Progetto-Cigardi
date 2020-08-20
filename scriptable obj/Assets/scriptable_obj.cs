e311672eae60369eacedb7b6995ae459c46fabf2
lorenzo <lorenzo.boldrini15345@gmail.com>
2020-08-20 09:12:45 +0200

    modifiche varie

---
+++ scriptable obj/Assets/scriptable_obj.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "new test", menuName = "Test")]
+public class scriptable_obj : ScriptableObject
+{
+
+
+}
+
