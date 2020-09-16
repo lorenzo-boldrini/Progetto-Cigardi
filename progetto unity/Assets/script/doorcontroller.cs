5f3abdb5924bef0de09ad3bb2dec231f4595138d
TeoNaba99 <56259499+TeoNaba99@users.noreply.github.com>
2020-09-16 12:53:25 +0200

    animazione porta

---
+++ progetto unity/Assets/script/doorcontroller.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class doorcontroller : MonoBehaviour
+{
+    Animator _Animator;
+    
+    void Start()
+    {
+        _Animator = GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            _Animator.SetBool("doorclosing", true);
+
+        }
+    }
+
+}
